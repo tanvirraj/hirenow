3b79a412c0835f6d7017470ccd5dad141a33b3c7
tanvir raj <rajbp02@gmail.com>
2016-02-09 16:29:55 +0600

    setup requiremenst file

--- uber/settings.py
+++ uber/settings.py
@@ -105,14 +105,16 @@ WSGI_APPLICATION = 'uber.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'uber',
+        'USER': 'tanvir',
+        'PASSWORD': 'tanvir',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
